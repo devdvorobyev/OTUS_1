8dbe45e68a48c05285c41467802f782612aba4b9
dvorobyev <dvorobyevdev@yandex.ru>
2023-11-06 21:42:47 +0300

    поменял моменты, на которые указал преподаватель

--- querySelector/index.js
+++ querySelector/index.js
@@ -1,48 +1,47 @@
 function getPath($el){
-    let currentElSelector = getAttrib($el),
+    if(typeof $el === 'object'){
+        let currentElSelector = getAttrib($el),
         parentSelector = getParentSelector($el.parentElement).reverse().join(' ');
 
-    function getAttrib($el) {
-        let attributeArray = [$el.tagName];
+        function getAttrib($el) {
+            let attributeArray = [$el.tagName];
 
-        for (const attribName in $el.getAttributeNames()) {
-            if($el.getAttributeNames()[attribName] === 'id'){
+            for (const attribName in $el.getAttributeNames()) {
+                if($el.getAttributeNames()[attribName] === 'id'){
 
-                return `#${$el.getAttribute($el.getAttributeNames()[attribName])}`;
+                    return `#${$el.getAttribute($el.getAttributeNames()[attribName])}`;
 
-            }else if($el.getAttributeNames()[attribName] === 'class'){
+                }else if($el.getAttributeNames()[attribName] === 'class'){
 
-                let classStr = ''
-                $el.getAttribute($el.getAttributeNames()[attribName]).split(' ').forEach(className => {
-                    if(!!className) classStr += '.'+className
-                });
-                attributeArray.push(classStr);
+                    let classStr = ''
+                    $el.getAttribute($el.getAttributeNames()[attribName]).split(' ').forEach(className => {
+                        if(!!className) classStr += '.'+className
+                    });
+                    attributeArray.push(classStr);
 
-            }else if($el.getAttributeNames()[attribName]){
-
-                attributeArray.push(`[${$el.getAttributeNames()[attribName]}="${$el.getAttribute($el.getAttributeNames()[attribName])}"]`);
-
+                }
             }
 
+            return attributeArray.join('');
+        };
+
+        function getParentSelector($el, parentSelector = []){
+            let parentSelectorStr = getAttrib($el)
+            parentSelector.push(parentSelectorStr)
+            if($el.parentElement){
+                parentSelector = getParentSelector($el.parentElement, parentSelector)
+            }
+            return parentSelector
         }
 
-        return attributeArray.join('');
-    };
+        function checkUnique(selector) {
+            return document.querySelectorAll(selector).length === 1;
+        };
 
-    function getParentSelector($el, parentSelector = []){
-        let parentSelectorStr = getAttrib($el)
-        parentSelector.push(parentSelectorStr)
-        if($el.parentElement){
-            parentSelector = getParentSelector($el.parentElement, parentSelector)
-        }
-        return parentSelector
+        return parentSelector + ' ' + currentElSelector;
+    }else{
+        return 'На вход был подан не Element DOM модели.'
     }
-
-    function checkUnique(selector) {
-        return document.querySelectorAll(selector).length === 1;
-    };
-
-    return parentSelector + ' ' + currentElSelector;
 }
 
 getPath($0)
