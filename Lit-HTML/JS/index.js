e4a8e63002fe3a59542ad6ad053f873a099cacc5
dvorobyev <dvorobyevdev@yandex.ru>
2023-11-20 23:30:46 +0300

    any

--- Lit-HTML/JS/index.js
+++ Lit-HTML/JS/index.js
@@ -1,7 +1,40 @@
-import {LitElement, html, css, join} from 'https://cdn.jsdelivr.net/gh/lit/dist@3/core/lit-core.min.js';
-/* import {join} from 'lit/directives/join.js'; */
+import {LitElement, html, css} from 'https://cdn.jsdelivr.net/gh/lit/dist@3/core/lit-core.min.js';
+
+const customTree = {
+  "name": 1,
+  "items": [{
+          "name": 2,
+          "items": [{ "name": 3 }, { "name": 4 }]
+      }, {
+          "name": 5,
+          "items": [{ "name": 6 }]
+      }]
+};
 
 export class MyTree extends LitElement {
+  // Define scoped styles right with your component, in plain CSS
+  static styles = css`
+    :host {
+      color: blue;
+    }
+  `;
+
+  constructor() {
+    super();
+    // Declare reactive properties
+
+  }
+
+  // Render the UI as a function of component state
+  render() {
+    return html`<ul>${customTree.name}<my-leaf></my-leaf><ul>`
+  }
+}
+
+customElements.define('my-tree', MyTree);
+
+
+export class Leaf extends LitElement {
   static properties = {
     customTree: {},
   };
@@ -15,46 +48,13 @@ export class MyTree extends LitElement {
   constructor() {
     super();
     // Declare reactive properties
-    this.customTree = {
-      "name": 1,
-      "items": [{
-              "name": 2,
-              "items": [{ "name": 3 }, { "name": 4 }]
-          }, {
-              "name": 5,
-              "items": [{ "name": 6 }]
-          }]
-    };
+
   }
 
   // Render the UI as a function of component state
   render() {
-    console.log(getTreeLeaves(this.customTree))
-    /* return getTreeLeaves(this.customTree) */
-    return html`${
-      join
-    }`
+    return html`<ul>${customTree.items ? html`${customTree.name}<my-tree></my-tree>` : customTree.name}<ul>`
   }
 }
 
-customElements.define('my-tree', MyTree);
-
-const getTreeLeaves = (obj, level = 1, renderArray = []) => {
-
-  if( level > 1 && !obj.hasOwnProperty('items')){
-    renderArray[renderArray.findIndex(el => el.includes('%s'))] = renderArray[renderArray.findIndex(el => el.includes('%s'))].replace('%s',`<ul><li>${obj.name}</li></ul>`)
-  }else if( level > 1 && obj.hasOwnProperty('items')) {
-    renderArray[renderArray.findIndex(el => el.includes('%s'))] = renderArray[renderArray.findIndex(el => el.includes('%s'))].replace('%s',`<ul><li>${obj.name} ${'%s'.repeat(obj.items.length)}</li></ul>`)
-  }else{
-    renderArray.push(`<ul><li>${obj.name} ${'%s'.repeat(obj.items.length)}</li></ul>`);
-  }
-  
-
-  if ( obj.hasOwnProperty('items') ) {
-    obj.items.forEach(element => {
-      getTreeLeaves(element, level + 1, renderArray)
-    });
-  }
-
-  return renderArray;
-}
+customElements.define('my-leaf', Leaf);
